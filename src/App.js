933a1873029dd03c44e25ddf05a080981fee3d49
zadragon <mase0804@naver.com>
2023-04-29 15:24:52 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const initState = ["투두리스트"];
+	const [state, setState] = useState(initState);
+	const [value, setValue] = useState("");
+
+	const changeInput = (e) => {
+		setValue(e.target.value);
+	};
+	const addList = () => {
+		setState((prevState) => [...prevState, value]);
+		setValue("");
+	};
+	return (
+		<div className="content">
+			<div>
+				<input type="text" onChange={changeInput} value={value} /> <button onClick={addList}>추가하기</button>
+			</div>
+			<div>
+				<h1>Todo List</h1>
+				<ul className="todolist">
+					{state?.map((item) => {
+						return <li>{item}</li>;
+					})}
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 export default App;
